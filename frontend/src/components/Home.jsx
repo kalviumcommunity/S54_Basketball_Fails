1f6367eee3c5cdcda2cf14a29e53681b168a9310
pranshupandey19 <pranshumac19@gmail.com>
2024-02-05 10:56:51 +0530

    landing page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+    <div className="home-center">
+      <div className="home-image">
+        <img
+          src="https://i.ytimg.com/vi/vvKthmera-o/maxresdefault.jpg"
+          alt=""
+        />
+      </div>
+      <div className="home-text">Worst <br />Plays In Nba History</div>
+      
+    </div>
+    <button className="explore">Explore</button>
+    </div>
+
+  );
+};
+
+export default Home;
